420c356f801cb7e4aeb1762bd80a4fef2e167069
rinch <rinchanruikun1650@gmail.com>
2024-02-01 22:07:58 +0900

    Fix: タスク記録機能

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -10,17 +10,20 @@ class RecordsController < ApplicationController
   def new
     @categories = Category.where(family_id: @family.id)
     @tasks = Task.where(family_id: @family.id)
+    @record = TaskUser.new
   end
 
   def create
     # 一括記録処理だよ。
-    tasks_params = params[:tasks]
-    tasks_params.each do |task_id, task_data|
+    tasks_params = params[:task_user][:tasks]
+    tasks_params.each do |task_id, task_count|
       id = task_id.to_i
-      count = task_data[:count].to_i
-      task = Task.find(id)
-      TaskUser.create!(task_id: id, user_id: current_user.id, family_id: @family.id, count: count)
-      current_user.update_column(:points, (current_user.points + task.points * count))
+      count = task_count[:count].to_i
+      if count > 0
+        task = Task.find(id)
+        TaskUser.create!(task_id: id, user_id: current_user.id, family_id: @family.id, count: count)
+        current_user.update_column(:points, (current_user.points + task.points * count))
+      end
     end
     @family.users.each do |user|
       user.update_column(:pocket_money, user.calculate_pocket_money)
@@ -32,18 +35,5 @@ class RecordsController < ApplicationController
   def task_index
     @category = Category.find(params[:id])
     @record = TaskUser.new
-  end
-
-  def increment
-    @task = Task.find(params[:id])
-    @record = TaskUser.new
-    @record.increment!(:count)
-    render turbo_frame: @task, partial: 'records/task_for_record', locals: { task: @task, record: @record }
-  end
-
-  private
-
-  def task_user_params
-    params.require(:task_user).permit(task_users_attributes: [:task_id, :count])
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+
+  default_scope -> { order(created_at: :asc) }
+
   has_many :task_users, dependent: :destroy
   has_many :users, through: :task_users
   has_many :approval_requests
